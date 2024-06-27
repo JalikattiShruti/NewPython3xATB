bad37879bbed6ed4c53ad094632ebeaa2b77289c
JalikattiShruti <shrutijalikatti117@gmail.com>
2024-06-28 01:52:01 +0530

    TASK2 Square and cube program Greater number program

---
+++ TASK/TASK_2 Square and Cube.py
@@ -0,0 +1,18 @@
+# Python script that calculates the square and cube of a given number.
+# num = 2 sq - 4, c = 8
+
+# Define the number
+num = 2
+
+# Calculate the square
+square = num ** 2
+
+# Calculate the cube
+cube = num ** 3
+
+# Print the results
+print(f"The square of {num} is {square}")
+print(f"The cube of {num} is {cube}")
+
+
+
---
+++ TASK/TASK_2 greater number.py
@@ -0,0 +1,14 @@
+#Create a program that takes two numbers as input and
+#prints whether the first number is greater than, less than, or
+#equal to the second number.
+
+# Input two numbers from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+elif num1 < num2:
+    print(f"{num1} is less than {num2}")
+else:
+    print(f"{num1} is equal to {num2}")
